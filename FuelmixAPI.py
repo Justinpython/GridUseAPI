67daa37269e2a5775e556c3ca627b873ef009272
Justinpython <47370439+Justinpython@users.noreply.github.com>
2020-02-14 14:21:45 +0000

    Update

--- FuelmixAPI.py
+++ FuelmixAPI.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
 
 response = requests.get('https://api.carbonintensity.org.uk/regional/scotland', params={})
 
-data = response.json()
+info= []
 
-jprint(data)
+pie = response.json()["data"][0]["data"][0]
+
+for i in pie["generationmix"]:
+	info.append(i)
+
+
+print("Energy production, Scotland")
+for i in info:
+	print(i["fuel"],": ", i["perc"],"%")
